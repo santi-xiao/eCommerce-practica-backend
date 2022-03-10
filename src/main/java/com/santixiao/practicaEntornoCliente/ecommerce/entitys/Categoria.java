a05ff888b3fee2181f28548b56f4c743b2620855
Santi Xiao <santi.xiao.rodriguez@gmail.com>
2022-03-10 02:34:39 +0100

    limpieza

--- src/main/java/com/santixiao/practicaEntornoCliente/ecommerce/entitys/Categoria.java
+++ src/main/java/com/santixiao/practicaEntornoCliente/ecommerce/entitys/Categoria.java
@@ -1,15 +1,12 @@
 package com.santixiao.practicaEntornoCliente.ecommerce.entitys;
 
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name="categoria")
